616189e4be1d9c24570ab6d8c6ee3576b79c91b4
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-02-14 12:59:54 +0600

    function added

---
+++ script/script.js
@@ -0,0 +1,50 @@
+// function playNow() {
+//     const homePage = document.getElementById('homePage');
+//     homePage.classList.add('hidden');
+//     const playGroundPage = document.getElementById('playGroundPage');
+//     playGroundPage.classList.remove('hidden');
+// }
+
+
+// উপরের কোড কমেন্ট করা হইছে কারন আমরা শুধু ফানশন দিয়ে কাজ করব
+// যার জন্য একটা ইউটিলিটি ক্লাস নিয়েছি। ঐ খানে শুধু ফানশন লিখব আর 
+// এই খানে কল করব 
+
+function playNow() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script/utility.js
@@ -0,0 +1,3 @@
+function hiddenById(elementId) {
+    const element
+}
