7731da1a820c0e2e0ca8ae19c0be3a29d0141a8c
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-02-14 17:15:10 +0600

    almost done

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+
 // function playNow() {
 //     const homePage = document.getElementById('homePage');
 //     homePage.classList.add('hidden');
@@ -7,16 +8,20 @@
 
 
 // উপরের কোড কমেন্ট করা হইছে কারন আমরা শুধু ফানশন দিয়ে কাজ করব
-// যার জন্য একটা ইউটিলিটি ক্লাস নিয়েছি। ঐ খানে শুধু ফানশন লিখব আর 
+// যার জন্য একটা ইউটিলিটি ক্লাস নিয়েছি। ঐ খানে শুধু ফানশন লিখব আর
 // এই খানে কল করব 
 
-function playNow() {
-
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+    const display = document.getElementById('display');
+    display.innerText = alphabet
 }
 
-
-
-
+function playNow() {
+    hiddenById('homePage');
+    showById('playGroundPage');
+    continueGame();
+}
 
 
 
--- script/utility.js
+++ script/utility.js
@@ -1,3 +1,21 @@
 function hiddenById(elementId) {
-    const element
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
 }
+function showById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+
+function getARandomAlphabet() {
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabet = alphabetString.split('');
+
+    const randomIndex = Math.round(Math.random() * 25);
+    return alphabet[randomIndex];
+}
+
+
+
+console.log();
